002e28b0b8e4287891a93244116f5f87dc2e7c4e
Luke Miller <lam59@buffalo.edu>
2023-04-03 23:48:38 -0400

    Initial commit

---
+++ server/Data/NotesDBContext.cs
@@ -0,0 +1,20 @@
+﻿//Acts as a bridge between the data and entity framework
+
+using System;
+using Microsoft.EntityFrameworkCore;
+using server.Models;
+
+namespace server.Data
+{
+    public class NotesDBContext : DbContext
+    {
+        // Constructor gets options and passes it to the base class
+        public NotesDBContext(DbContextOptions options) : base(options)
+        { 
+        }
+
+        // Same name of table that's created as part of EntityFrameworkCore
+        public DbSet<Note> Notes { get; set; }
+    }
+}
+
---
+++ server/Program.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.EntityFrameworkCore;
+using server.Data;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+
+builder.Services.AddControllers();
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+// Injected the NotesDBContext inside of the Services collection.
+// Can now use it inside the controlllers.
+builder.Services.AddDbContext<NotesDBContext>(options =>
+{
+    options.UseSqlServer(builder.Configuration.GetConnectionString("NotesAppConnectionString"));
+});
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
+
